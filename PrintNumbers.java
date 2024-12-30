4be216e2643b70f0f6f897b28dff8cf37f869b42
adityatakte <adityatakte5@gmail.com>
2024-12-30 02:53:50 -0800

    Update PrintNumbers.java

--- PrintNumbers.java
+++ PrintNumbers.java
@@ -1,7 +1,7 @@
 public class PrintNumbers {
     public static void main(String[] args) {
         // Loop from 1 to 10 and print each number
-        for (int i = 1; i <= 10; i++) {
+        for (int i = 5; i <= 20; i++) {
             System.out.println(i);
         }
     }
